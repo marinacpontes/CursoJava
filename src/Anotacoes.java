51f191175e39e672a5f582867f34e9ab686e1749
mrnpnts <marinacarneiropontes@gmail.com>
2026-04-25 22:35:02 -0300

    primeiro commit

---
+++ src/Anotacoes.java
@@ -0,0 +1,23 @@
+public class Anotacoes {
+/*
+    para fazer operações:
+    public static void main(String[] args) {
+        int a = 10; int b =5;
+        System.out.println(a + b); // soma
+        System.out.println(a - b); // subtração
+        System.out.println(a * b); // multiplicação
+        System.out.println(a / b); // divisão inteira
+        System.out.println(a % b); // resto da divisão
+
+        "==" igual a
+        "!=" diferente de
+        ">" maior que
+        ">=" maior ou igual a
+        "<" (menor que)
+        "<=" menor ou igual a
+    }
+
+
+
+*/
+}
